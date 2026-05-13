cb88349790d2743020515c511e6c66d29dfb6e2f
LeonardTarigan <leontrg32@gmail.com>
2026-05-13 21:43:39 +0700

    fix: add path to dashboard card

--- fe/src/app/dashboard/(index)/page.tsx
+++ fe/src/app/dashboard/(index)/page.tsx
@@ -6,46 +6,58 @@ import formatRelativeTime from "@/shared/lib/helpers/formatRelativeTime";
 import { ArrowRight02Icon } from "@hugeicons/core-free-icons";
 import { HugeiconsIcon } from "@hugeicons/react";
 import useQueryContract from "./hooks/use-query-contracts";
+import Link from "next/link";
+import { APP_PATH } from "@/shared/constants/app-path";
 
 const DashboardPage = () => {
   const { data, isLoading } = useQueryContract();
 
   const renderCards = () =>
     data?.map(
-      ({ uuid, name, status, gas_eslimate, audits, updated_at, language }) => (
-        <div key={uuid} className="rounded-lg border bg-mist-900/50 p-3">
-          <div className="flex justify-between">
-            <div>
-              <h3 className="line-clamp-1 font-bold">{name}</h3>
+      ({ uuid, name, status, gas_eslimate, audits, updated_at, language }) => {
+        const source = audits ? audits[0].kind : null;
+        let targetPath: string | null = null;
+
+        if (source === "codegen") targetPath = APP_PATH.dashboard.codeGen;
+        if (source === "audit") targetPath = APP_PATH.dashboard.codeGen;
+
+        return (
+          <div key={uuid} className="rounded-lg border bg-mist-900/50 p-3">
+            <div className="flex justify-between">
+              <div>
+                <h3 className="line-clamp-1 font-bold">{name}</h3>
+                <p className="text-xs text-mist-500">
+                  <span className="capitalize">{language}</span>
+                </p>
+              </div>
+              <Badge variant="outline" className="capitalize">
+                {status}
+              </Badge>
+            </div>
+            <div className="my-4 grid grid-cols-2 border-y py-3">
+              <div>
+                <h4 className="text-xs text-mist-500">Audits</h4>
+                <p className="font-bold">{audits?.length ?? 0}</p>
+              </div>
+              <div>
+                <h4 className="text-xs text-mist-500">Gas</h4>
+                <p className="font-bold">{gas_eslimate ?? "-"}</p>
+              </div>
+            </div>
+            <div className="flex items-end justify-between">
               <p className="text-xs text-mist-500">
-                <span className="capitalize">{language}</span>
+                Edited {formatRelativeTime(updated_at)} ago
               </p>
-            </div>
-            <Badge variant="outline" className="capitalize">
-              {status}
-            </Badge>
-          </div>
-          <div className="my-4 grid grid-cols-2 border-y py-3">
-            <div>
-              <h4 className="text-xs text-mist-500">Audits</h4>
-              <p className="font-bold">{audits?.length ?? 0}</p>
-            </div>
-            <div>
-              <h4 className="text-xs text-mist-500">Gas</h4>
-              <p className="font-bold">{gas_eslimate ?? "-"}</p>
+              <Link href={`${targetPath}/${uuid}`}>
+                <Button size="xs" variant="outline">
+                  <span>Open</span>
+                  <HugeiconsIcon icon={ArrowRight02Icon} strokeWidth={2} />
+                </Button>
+              </Link>
             </div>
           </div>
-          <div className="flex items-end justify-between">
-            <p className="text-xs text-mist-500">
-              Edited {formatRelativeTime(updated_at)} ago
-            </p>
-            <Button size="xs" variant="outline">
-              <span>Open</span>
-              <HugeiconsIcon icon={ArrowRight02Icon} strokeWidth={2} />
-            </Button>
-          </div>
-        </div>
-      ),
+        );
+      },
     );
 
   const renderSkeleton = () =>
